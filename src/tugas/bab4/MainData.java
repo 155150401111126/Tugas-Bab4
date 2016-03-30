06df9919443f38135735cef0d995dc0ca011cf3b
155150401111126 <bagoes_g01@yahoo.co.id>
2016-03-30 22:35:00 +0700

    Added files via upload

---
+++ src/tugas/bab4/MainData.java
@@ -0,0 +1,10 @@
+package Praktikum;
+public class MainData {
+    public static void main(String[] args) {
+        
+        Data satu = new Data ();
+        satu.input();
+        satu.tabel();
+
+    }
+}
